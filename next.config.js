2d1bb2889d5cdcbc643bd0f92905258d5c79570b
Gun Khemnak <gunnn_boy_hand@hotmail.com>
2025-08-29 11:01:49 +0700

    add env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? 'http://10.1.0.155:3000' : '',
 }
 
 module.exports = nextConfig

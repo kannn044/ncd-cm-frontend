46cc1edf4142e9dc60e84938517e64b494c12cb4
Gun Khemnak <gunnn_boy_hand@hotmail.com>
2025-08-29 13:46:53 +0700

    edit login

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  assetPrefix: isProd ? 'http://10.1.0.155:3000' : '',
+  swcMinify: true
 }
 
 module.exports = nextConfig
